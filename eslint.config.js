a0115b878c3061474c9a94db65f2d1fc77a03fbd
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2024-11-29 09:23:24 +0500

    File changing

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,15 @@
-export default {
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: [
-      "eslint:recommended",
-    ],
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    rules: {
-    },
-  };
-  
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+  },
+};
