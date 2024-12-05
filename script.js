d3befd68b859ab74eea638cbebdf9859b9a8f1c7
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2024-12-05 11:42:58 +0500

    Added responsiveness and JS

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Starting of the file!
+const menuToggle = document.getElementById("menuToggle");
+  const menuItems = document.getElementById("menuItems");
+  const spans = menuToggle.querySelectorAll("span");
+
+  menuToggle.addEventListener("click", () => {
+    menuItems.classList.toggle("hidden");
+    menuItems.classList.toggle("flex");
+
+    spans[0].classList.toggle("rotate-45");
+    spans[0].classList.toggle("translate-y-2");
+    spans[1].classList.toggle("opacity-0");
+    spans[2].classList.toggle("-rotate-45");
+    spans[2].classList.toggle("-translate-y-2");
+  });
