88a351f1f0feeefbdab6695bbecb0c080f04b980
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2024-12-23 14:39:03 +0500

    Links modified with search icon animation
    Links properly switching color on hover and the search icon is perfectly rotating on hover covering a smooth transition.

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ const menuToggle = document.getElementById("menuToggle");
     spans[2].classList.toggle("-rotate-45");
     spans[2].classList.toggle("-translate-y-2");
   });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.nav-item');
+  
+    links.forEach((link, index) => {
+      link.addEventListener('mouseover', () => {
+        links.forEach((otherLink, otherIndex) => {
+          if (index === 0 && otherIndex !== 0) {
+            otherLink.classList.add('hover-gray');
+          } else if (index !== 0 && otherIndex !== index) {
+            otherLink.classList.add('hover-gray');
+          }
+        });
+      });
+  
+      link.addEventListener('mouseout', () => {
+        links.forEach((otherLink) => {
+          otherLink.classList.remove('hover-gray');
+        });
+      });
+    });
+  });
