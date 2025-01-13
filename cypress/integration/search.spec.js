f115b1ad56a52230e6303fdf73a3724c25eafb80
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-01-13 22:07:41 +0500

    Deleted cypress files

--- cypress/integration/search.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Search Animation', () => {
-    it('should display search results when searching', () => {
-      cy.visit('http://localhost:3000');
-      cy.get('input[type="search"]').type('Hello');
-      cy.get('.search-results').should('be.visible');
-    });
-  });
-  
